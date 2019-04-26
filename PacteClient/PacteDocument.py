38922f8eb20981d8bbbfd0791c155bae8b5df315
Lise Rebout <lise.rebout@crim.ca>
2019-04-26 15:02:57 -0400

    Adding PacteCorpusreader

--- PacteClient/PacteDocument.py
+++ PacteClient/PacteDocument.py
@@ -3,12 +3,12 @@
 class PacteDocument:
 
     def __init__(self, id, title, content, source, language,
-			docSize=None, dateAdded=None, path=None):
-            self.id = id
-            self.title = title
-            self.content = content
-            self.source = source
-            self.language = language
-            self.docSize = docSize
-            self.dateAdded = dateAdded
-            self.path = path
+                 docSize=None, dateAdded=None, path=None):
+        self.id = id
+        self.title = title
+        self.content = content
+        self.source = source
+        self.language = language
+        self.docSize = docSize
+        self.dateAdded = dateAdded
+        self.path = path
