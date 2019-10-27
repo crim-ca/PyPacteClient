32f8c024e3eefd653e154707c4f09f3292369800
Pierre André Ménard <pierre-andre.menard@crim.ca>
2019-10-27 13:20:00 -0400

    Lecture document du disque

--- PacteClient/PacteDocument.py
+++ PacteClient/PacteDocument.py
@@ -1,14 +1,30 @@
+import json
 
 
 class PacteDocument:
 
-    def __init__(self, id, title, content, source, language,
-			docSize=None, dateAdded=None, path=None):
-            self.id = id
-            self.title = title
-            self.content = content
-            self.source = source
-            self.language = language
-            self.docSize = docSize
-            self.dateAdded = dateAdded
-            self.path = path
+    def __init__(self, id, title, content, source, language, doc_size=None, date_added=None, path=None):
+        self.id = id
+        self.title = title
+        self.content = content
+        self.source = source
+        self.language = language
+        self.docSize = doc_size
+        self.dateAdded = date_added
+        self.path = path
+
+    @classmethod
+    def from_file(cls, filepath):
+        try:
+            input = json.loads(open(filepath).read())
+            id = input["id"]
+            title = input["title"]
+            content = input["text"]
+            source = input["source"]
+            language = input["language"]
+            doc_size = len(content)
+
+            return cls(id, title, content, source, language, doc_size, None, None)
+        except:
+            print("Error reading document file")
+            return
